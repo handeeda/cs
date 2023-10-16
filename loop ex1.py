b8bff859a5376a645ffce32bb1bf7c246898bf80
handeeda <144910880+handeeda@users.noreply.github.com>
2023-10-16 11:57:12 +0100

    Add files via upload

---
+++ loop ex1.py
@@ -0,0 +1,39 @@
+
+total=0
+while total <=50:
+    number=int(input("enter a number"))
+    total=number+total
+    print ("the total is",total,)
+   
+
+number=0
+while number<5:
+    print(number)
+    number=int(input("enter a number"))
+if number>=5:
+     print("the last number you entered was",number,)
+   
+number1=int(input("enter a number"))
+number2=int(input("enter a number again"))
+total=number1+number2
+msg=input("wanna add another number")
+
+while msg=="y":
+    number3=int(input("enter another number"))
+    total=total+number3
+    print(total)
+else:
+    print(total)
+
+
+
+    
+    
+        
+ 
+ 
+
+
+
+
+

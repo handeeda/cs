1d82bbbce141cf4161b2d38f266b11c552c67ad3
handeeda <144910880+handeeda@users.noreply.github.com>
2023-12-10 21:49:59 +0000

    Add files via upload

---
+++ christmas revision1.py
@@ -0,0 +1,46 @@
+"""
+name= input("write your name")
+print("hello",name)
+
+first=int(input("write a value"))
+second= int(input("write another value"))
+a= first+second
+print(a)
+
+
+c= int(input("write a degree"))
+f= 9/5
+f=f*c
+f=f+35
+print("degree as fahrenheit is",f)
+
+principle= int(input("write a principle"))
+rate= int(input("write a rate"))
+time= int(input("write a time"))
+simplei=principle*rate
+simplei=simplei*time
+simplei=simplei/100
+print("simple interest is",simplei)
+
+seconds1=int(input("enter seconds"))
+hours=seconds1/60
+hours=hours/60
+print(hours)
+
+"""
+a=int(input("enter a variable for a"))
+b=int(input("enter a variable for b"))
+pow(a,b)
+print("a is",a)
+print("b is",b)
+
+
+
+
+
+
+
+
+
+
+

941a77a7e04967373afae3bb36a7f7010272a734
handeeda <144910880+handeeda@users.noreply.github.com>
2023-12-13 12:54:43 +0000

    Add files via upload

--- christmas revision1.py
+++ christmas revision1.py
@@ -1,39 +1,64 @@
 """
-name= input("write your name")
-print("hello",name)
-
-first=int(input("write a value"))
-second= int(input("write another value"))
-a= first+second
-print(a)
-
-
-c= int(input("write a degree"))
-f= 9/5
-f=f*c
-f=f+35
-print("degree as fahrenheit is",f)
-
-principle= int(input("write a principle"))
-rate= int(input("write a rate"))
-time= int(input("write a time"))
-simplei=principle*rate
-simplei=simplei*time
-simplei=simplei/100
-print("simple interest is",simplei)
-
-seconds1=int(input("enter seconds"))
+seconds1= int(input("write seconds"))
 hours=seconds1/60
 hours=hours/60
+hours=int(hours)
+
+minutes=seconds1/60
+hours2=hours*60
+minutes=minutes-hours2
+minutes=int(minutes)
+
+seconds1=seconds1-(hours*60*60+minutes*60)
+seconds=seconds1
+
+int3=int(seconds)
 print(hours)
+print(minutes)
+print(seconds)
+
+
+a=int(input("write the first variable for a"))
+b=int(input("write the second variable for b"))
+
+radius=int(input("write a radius of a circle"))
+
+a=3
+powerradius=pow(radius,2)
+area=a*powerradius
+circumference=2*radius*a
+circumference=circumference*area
+print("area is",area)
+print("circumference is",circumference)
+
+
+print("write the lengths")
+a=int(input("write a"))
+b=int(input("write b"))
+c=int(input("write c"))
+s=a+b+c
+s=s/2
+a=s-a
+a=a*s
+b=s-b
+b=b*s
+c=s-c
+c=c*s
+area=a+b+c
+area=pow(area,2)
+print(area)
+
+principle=int(input("write the principle"))
+rate=int(input("write the rate"))
+time=int(input("write the time"))
+ci=rate/100
+ci=ci+1
+ci=pow(ci,time)
+ci=principle*ci
+ci=ci-principle
+print("compund interest is",ci)
 
 """
-a=int(input("enter a variable for a"))
-b=int(input("enter a variable for b"))
-pow(a,b)
-print("a is",a)
-print("b is",b)
-
 
 
 
---
+++ christmas revision2.py
@@ -0,0 +1,75 @@
+"""
+num=int(input("write a number"))
+
+if num%2==0:
+
+    print("even")
+else:
+    print("odd")
+
+
+
+age=int(input("write your age"))
+if age>=18:
+    print("you can vote")
+else:
+    print("you cant vote yet")
+
+
+first=int(input("write a number"))
+second=int(input("write another number"))
+if first>second:
+    print("first is bigger")
+elif second>first:
+    print("second is bigger")
+else:
+    print("they are equal")
+
+
+num=int(input("write a number"))
+if num>0:
+    print(num,"is bigger than 0")
+elif num<0:
+    print(num,"smaller than 0")
+else:
+    print(num,"is equal to 0")
+
+age=int(input("write your age"))
+if age>=0 and age<=12:
+    print("child")
+elif age>12 and age<=19:
+    print("teenager")
+elif age>19 and age<=59:
+    print("adult")
+else:
+    print("senior citizen")
+    
+
+num=int(input("write a number"))
+if num==1:
+    print("sunday")
+elif num==2:
+    print("monday")
+elif num==3:
+    print("tuesday")
+elif num==4:
+    print("wednesday")
+elif num==5:
+    print("thursday")      
+elif num==6:
+    print("friday")
+elif num==7:
+    print("saturday")
+else:
+    print("error")
+
+"""
+
+
+
+
+
+
+
+
+

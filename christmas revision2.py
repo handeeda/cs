6fb0728db335bd1ff5d38b93a3fa5bdd542e3c04
handeeda <144910880+handeeda@users.noreply.github.com>
2023-12-15 12:15:27 +0000

    Add files via upload

--- christmas revision2.py
+++ christmas revision2.py
@@ -1,32 +1,67 @@
-"""
-weight=int(input("write your weight in kg"))
-height=float(input("write your height in m"))
-bmi=height*height
-bmi=weight/bmi
-if bmi <=18.5:
-    print("underweight")
-elif bmi>18.5 and bmi<=24.9:
-    print("normal weight")
-elif bmi>25 and bmi<=29.9:
-    print("overweight")
+"""import math
+a= int(input("enter the value of a"))
+b= int(input("enter the value of b"))
+c= int(input("enter the value of c"))
+formula=b**2-4*a*c
+formula=math.sqrt(formula)
+minus=formula+b
+plus=formula-b
+firstanswer=minus/2*a
+secondanswer=plus/2*a
+equation=b**2-4*a*c
+if equation==0:
+    print("two equal roots")
+elif equation<0:
+    print("two real roots")
 else:
-    print("obesity")
+    print("two complex roots")
+print(firstanswer)
+print(secondanswer)
 
 
-grade=int(input("write your grade"))
-if grade<=100 and grade>=90:
-    print("A")
-elif grade<90 and grade>=80:
-    print("B")
-elif grade<80 and grade>=70:
-    print("C")
-elif grade<70 and grade>=60:
-    print("D")
-elif grade<60:
-    print("F")
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+num3=int(input("enter the third number"))
+list1=[num1,num2,num3]
+
+list1.sort()
+print(list1)
+
+
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+num3=int(input("enter the third number"))
+list1=[num1,num2,num3]
+
+list1.sort()
+largest=list1[2]
+print(largest)
+
+
+string1=input("enter a character:")
+vowels=["a","e","i","o","u"]
+if string1 in vowels:
+    print("vowel")
 else:
-    print("there is no such a grade")
+    print("consonant")
+
+
+year=input("enter a year")
+listed=list(year)
+sum1=0
+for i in listed:
+    sum1=sum1+int(i)
+#int1= int(listed)
+
+#sum1=sum(int1)
+sum1=sum1/4
+if sum1==0:
+    print("leap year")
+else:
+    print("not a leap year")
 """
+
+
 
 
 

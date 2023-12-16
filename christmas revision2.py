bbbfa141465faad76529375501548ceef7620548
handeeda <144910880+handeeda@users.noreply.github.com>
2023-12-16 23:02:17 +0000

    Add files via upload

--- christmas revision2.py
+++ christmas revision2.py
@@ -1,66 +1,32 @@
-"""import math
-a= int(input("enter the value of a"))
-b= int(input("enter the value of b"))
-c= int(input("enter the value of c"))
-formula=b**2-4*a*c
-formula=math.sqrt(formula)
-minus=formula+b
-plus=formula-b
-firstanswer=minus/2*a
-secondanswer=plus/2*a
-equation=b**2-4*a*c
-if equation==0:
-    print("two equal roots")
-elif equation<0:
-    print("two real roots")
-else:
-    print("two complex roots")
-print(firstanswer)
-print(secondanswer)
-
-
-num1=int(input("enter the first number"))
-num2=int(input("enter the second number"))
-num3=int(input("enter the third number"))
-list1=[num1,num2,num3]
-
-list1.sort()
-print(list1)
-
-
-num1=int(input("enter the first number"))
-num2=int(input("enter the second number"))
-num3=int(input("enter the third number"))
-list1=[num1,num2,num3]
-
-list1.sort()
-largest=list1[2]
-print(largest)
-
-
-string1=input("enter a character:")
-vowels=["a","e","i","o","u"]
-if string1 in vowels:
-    print("vowel")
-else:
-    print("consonant")
-
-
-year=input("enter a year")
-listed=list(year)
-sum1=0
-for i in listed:
-    sum1=sum1+int(i)
-#int1= int(listed)
-
-#sum1=sum(int1)
-sum1=sum1/4
-if sum1==0:
-    print("leap year")
-else:
-    print("not a leap year")
 """
+for i in range(1,11):
+ print(i)
 
+for i in range(20,1,-1):
+    print(i)
+
+
+for num in range(1, 11):
+    if num % 2 == 0:
+        print(num)
+
+n=int(input("print a number"))
+for i in range(1,n+1):
+    print(i)
+
+n=int(input("print a number"))
+for i in range(1,n+1,2):
+    print(i)
+
+
+for i in range(3):
+    i="happy bırthday"
+    print(i)
+"""
+n = int(input("Enter the value of n: "))
+
+for i in range(1, n + 1):
+    print(i**2)
 
 
 

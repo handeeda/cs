fe27bbc2869519ae1063053014372199598ee3d4
handeeda <144910880+handeeda@users.noreply.github.com>
2024-01-22 11:57:07 +0000

    Add files via upload

--- texts1.py
+++ texts1.py
@@ -38,7 +38,41 @@ if menu=="db":
 if menu=="bb":
     b1=int(input("write a binary number"))
     b2=int(input("write another binary number"))
-      
+    strb1=str(b1)
+    strb2=str(b2)
+    listedb1=list(strb1)
+    listedb2=list(strb2)
+    
+    a=1+0
+    b=0+1
+    c=0+0
+    d=1+1
+    carry=0
+    list2=[]
+    num=-1
+    for i in listedb1:
+        
+        result=int(listedb1[num])+int(listedb2[num])+carry
+        if result ==0:
+            carry=0
+            list2.append(0)
+        elif result==1:
+            carry=0
+            list2.append(1)
+        
+        print(result)
+        num=num-1
+        if result==a or result==b:
+            list2.append(1)
+            
+        elif result==c:
+            list2.append(0)
+        elif result==2:
+            carry=carry+1
+            list2.append(0)
+            
+    print(list2)       
+     
       
       
       

11c50bcf4e15146c3ecbc313164b6dfc727f90e7
handeeda <144910880+handeeda@users.noreply.github.com>
2024-01-31 13:11:08 +0000

    Add files via upload

--- texts1.py
+++ texts1.py
@@ -1,6 +1,6 @@
-import functions
+#import functions
 print("welcome to the binary numbers program \n please choose from the menu")
-menu=input("\n\nbinary to decimal\ndecimal to binary\nadd two binary numbers\nbinary to hexidecimal\nhexidecimal to binary")
+menu=input("\n\nbinary to decimal\ndecimal to binary\nadd two binary numbers\nbinary to hexidecimal\nhexidecimal to binary\nbinary to hexidecimal")
 
 def db(decimal):
     list2=[]
@@ -79,30 +79,70 @@ if menu=="bb":
     print(list2)       
 
 if menu=="hb":
+    
+    
     A=10
     B=11
     C=12
     D=13
     E=14
+    list3=["A","B","C","D","E"]
+    print(list3)
     hexinum=input("write a hexidecimal number")
-    
+    print(list3)
     listedhexinum=list(hexinum)
-    inthexinum=int(hexinum)
-   
-    num=0
-    
+    list4=[]
     list2=[]
-    while inthexinum>0:
-        remainder=listedhexinum[num]%2
-        ans=listedhexinum[num]//2
-        list2.append(remainder)
-        list2.append(ans)
-        num=num+1
-    print(list2)         
+    num=0
+    for i in listedhexinum:
+        if i in list3:
+            if i =="A":
+             list2.append(10)  
+            if i =="B":
+              list2.append(11)
+            if i =="C":
+                list2.append(12)
+            if i =="D":
+                list2.append(13)
+            if i =="E":
+                list2.append(14)
+            if i =="F":
+                list2.append(15)
+        else:
+            list2.append(int(i))
+    print(list2)
+    #strhexinum=str(listedhexinum)    
+    #inthexinum=int(strhexinum)
+    list5=[]
+    for i in list2:
+        print('-------------------')
+        print(i)
+        print('-------------------')
+        while i>0:
+            remainder=i%2
+            ans=i//2
+            list4.append(remainder)
+            #list4.append(ans)
+            i=ans
+        if len(list4)<4:
+            list4.append(0)
+            
+        print(list4)         
         
-        
-         
     
+        list5.append(list4)
+        list4=[]
+    print(list5)
+         
+if menu== "bh":
+    binarynum=input("enter the binry number")
+    #strbinarynum=str(binarynum)
+    listedbinarynum=list(binarynum)
+    print(listedbinarynum)
+    for i in listedbinarynum:
+        
+        fourr=listedbinarynum[-1]
+        print(fourr)
       
       
       

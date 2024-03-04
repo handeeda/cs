46a28cde84671f847f006ac533cd9a5e333bcc75
handeeda <144910880+handeeda@users.noreply.github.com>
2024-03-04 11:54:58 +0000

    Add files via upload

--- logicgates.py
+++ logicgates.py
@@ -29,7 +29,7 @@ def XNOR_gate (a,b):
             else:
                 return 0            
             
-def NOR_gate (a,b):
+def XOR_gate (a,b):
             if a==0 and b==0:
                 return 0
             elif a==1 and b==1:
@@ -45,7 +45,35 @@ def NAND_gate (a,b):
                 return 1            
             
             
-            
+def half_adder(a,b):
+    summ=XOR_gate(a,b)
+    carry=AND_gate(a,b)
+    return(summ,carry)
+h=half_adder(a,b)
+
+def full_adder (a,b,carry_in):
+    h(a,b)
+    h(carry_in,summ)
+    d=OR_gate(carry,carry)
+    return(summ,d)
+f=full_adder(a,b,carry_in)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             
             
             

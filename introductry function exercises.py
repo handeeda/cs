57c999cca8778ed715efe5bce3848aaf8fd43a5b
handeeda <144910880+handeeda@users.noreply.github.com>
2024-03-22 12:36:17 +0000

    Add files via upload

--- introductry function exercises.py
+++ introductry function exercises.py
@@ -56,6 +56,7 @@ print(totals(c[0],c[1]))
 
 """
 sentence=input("write a sentence:")
+"""
 list1=[]
 def sentence_to_decimal(a,b):
     
@@ -88,22 +89,94 @@ for i in sd:
     list3.append(decimal_to_binary(i))
 print(list3)
 
-for i in list3:
-    lenofi=len(i)
+#for i in list3:
+    stri=str(i)
+    listedi=list(stri)
+    lenofi=len(listedi)
     if lenofi <=7:
-        i.reverse()
-        i.append(0)
-        i.reverse()
+        listedi.reverse()
+        listedi.append(0)
+        listedi.reverse()
     if lenofi>7 and lenofi<=11:
-        i.reverse()
-        i[:6].append(0)
-        i[:6].append(1)
-        i.reverse()
+        listedi.insert(5,0)
+        listedi.insert(5,1)
+        listedi.reverse()
+        listedi.append(0)
+        listedi.append(1)
+        listedi.append(1)
+        listedi.reverse()
+    if lenofi>11 and lenofi<=16:
+        listedi.insert(10,0)
+        listedi.insert(10,1)
+        listedi.insert(4,0)
+        listedi.insert(4,1)
+        listedi.reverse()
+        listedi.append(0)
+        listedi.append(1)
+        listedi.append(1)
+        listedi.append(1)
+        listedi.reverse()
+    if lenofi>16:
+        listedi.insert(15,0)
+        listedi.insert(15,1)
+        listedi.insert(9,0)
+        listedi.insert(9,1)
+        listedi.insert(3,0)
+        listedi.insert(3,1)
+        listedi.reverse()
+        listedi.append(0)
+        listedi.append(1)
+        listedi.append(1)
+        listedi.append(1)
+        listedi.append(1)
+        listedi.reverse()
+print(list3)            
+ """   
+
+def utf_to_binary(listt):
+    #for i in listt:
+        listedi=list(listt)
+        lenofi=len(listedi)
+        print(lenofi)
+        if lenofi <=8:
+            listedi.remove(listedi[0])
+        elif lenofi>8 and lenofi<=16:
+            output = listedi[:2] +listedi[12:16]
+            del listedi[:2]
+            #listedi.remove(listedi[0])
+            #listedi.remove(listedi[1])
+            #listedi.remove(listedi[2])
+            del listedi[4:5]
+            #listedi.remove(listedi[7])
+            #listedi.remove(listedi[8])
             
-    
-
-
-
+        if lenofi>16 and lenofi<=24:
+            output = listedi[4:7] +listedi[12:16]
+            #listedi.remove(listedi[0])
+            #listedi.remove(listedi[1])
+            #listedi.remove(listedi[2])
+            #listedi.remove(listedi[3])
+            del listedi[3:4]
+            #listedi.remove(listedi[7])
+            #listedi.remove(listedi[8])
+            listedi.remove(listedi[15])
+            listedi.remove(listedi[16])
+        if lenofi>24:
+            listedi.remove(listedi[0])
+            listedi.remove(listedi[1])
+            listedi.remove(listedi[2])
+            listedi.remove(listedi[3])
+            listedi.remove(listedi[4])
+            listedi.remove(listedi[7])
+            listedi.remove(listedi[8])
+            listedi.remove(listedi[15])
+            listedi.remove(listedi[16])
+            listedi.remove(listedi[23])
+            listedi.remove(listedi[24])
+        return listedi
+print(utf_to_binary(sentence))        
+        
+1100100001010101
 
 
 

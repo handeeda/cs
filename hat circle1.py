618e8806cb67135f103f03fb31eff8e2407a1864
handeeda <144910880+handeeda@users.noreply.github.com>
2024-04-29 11:57:09 +0100

    Add files via upload

---
+++ hat circle1.py
@@ -0,0 +1,10 @@
+import random
+people=int(input("Hat Cirle\nwrite the amount of people"))
+for i in range (1,people+1):
+    persons_hat=[]
+    persons_hat.append(i)
+    persons_hat.append(random.randint(0,1))
+print("these are the people with their hat number\n",persons_hat)
+
+
+

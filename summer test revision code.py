772d583f6bdf8d1380508b84cbcc7aef904bd96e
handeeda <144910880+handeeda@users.noreply.github.com>
2024-05-15 13:14:05 +0100

    Add files via upload

---
+++ summer test revision code.py
@@ -0,0 +1,81 @@
+"""
+nums=input("write 5 numbers:")
+nums=nums.split()
+#print(type(nums))
+while len(nums)<5 or len(nums)>5:
+    nums=input("numbers are less or greater than 5\nwrite 5 numbers:")
+    print(nums)
+    nums=nums.split()
+    #print(type(nums))
+    
+for i in range(len(nums)):
+    nums[i]=int(nums[i])
+print(nums)
+
+a=0
+for i in nums:
+    i=i+1
+    nums[a]=i
+    a=a+1
+print(nums)
+
+
+hours=[12,7,9,9,6,8,2]
+milk=0.5
+milk_cost=1.35
+all_a=[]
+
+for h in hours:
+    a=0
+        
+    a=h*milk
+    a=a*milk_cost
+    all_a.append(a)
+
+all_a=sum(all_a)
+
+all_a=round(all_a,1)
+print(all_a)
+
+
+day=1
+rainfall_cm_list=[]
+while day!=8:
+    print("write the daily rainfall cm for day",day)
+    rainfall_cm=float(input("write here:"))
+    rainfall_cm_list.append(rainfall_cm)
+    day=day+1
+total=sum(rainfall_cm_list)
+print("the total rainfall cm is",total)
+
+len_rainfall=len(rainfall_cm_list)
+average=total/len_rainfall
+average=round(average,2)
+print("the average of rainfall cm is",average)
+
+
+for i in rainfall_cm_list:
+    if i>3.5:
+        exceeded_amount=i-3.5
+        exceeded_amount=round(exceeded_amount,2)
+        print("the exceeded amount for",i,"is",exceeded_amount)
+    else:
+        continue
+    
+"""    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    

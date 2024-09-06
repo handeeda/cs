e22e9ca5b12fb69db5b0261e4cf64e66a3a79cd4
handeeda <144910880+handeeda@users.noreply.github.com>
2024-09-06 12:55:51 +0100

    Add files via upload

--- project preperation 1.py
+++ project preperation 1.py
@@ -9,23 +9,56 @@ def median(num):
     cal=summ/2
     return cal
 
-def mode():
-    for i in all_numbers:
-        c=all_numbers.count(i)
+def mode(num):
+    for i in num:
+        c=num.count(i)
         if c>1:
+            cal=c
+            return cal
+        
+def frequency(num):
+    f=float(input("write a frequency:"))
+    length=len(num)
+    cal=f/length
+    return cal
+
+def top5(num):
+    list1=[]
+    num.sort()
+    num.reverse()
+    for i in num:
+        list1.append(i)
+        if len(list1)==5:
+            return list1
+    
+    
+    
+
+def last5(num):
+    list1=[]
+    num.sort()
+    for i in num:
+        list1.append(i)
+        if len(list1)==5:
+            return list1
+    
+    
+        
+    
             
         
         
     
 
-all_numbers=[]        
-menu=int(input("1.mean\n2.median\n3.mode\nchoose from the menu:"))
+all_numbers=[]
+menu=int(input("1.mean\n2.median\n3.mode\n4.frequency\n5.top 5 numbers\n6.last 5 numbers\nchoose from the menu:"))
 print("please enter youre numbers\nonly 20 or less:")
 if menu==1:
     for i in range (20):
         
         number=input("please enter number\nif youre done write stop:")
         if number=="stop":
+            
             result=mean(all_numbers)
             print(result)
             break
@@ -56,6 +89,64 @@ if menu ==3:
     for i in range(20):
         number=input("please enter number\nif youre done write stop:")
         if number=="stop":
+            result=mode(all_numbers)
+            print(result)
+            break
+        if number.isdigit():    
+            all_numbers.append(float(number))
+        
+        else:
+            print("error")
+
+if menu==4:
+    for i in range(20):
+        number=input("please enter number\nif youre done write stop:")
+        if number=="stop":
+            result=frequency(all_numbers)
+            print(result)
+            break
+        if number.isdigit():    
+            all_numbers.append(float(number))
+        
+        else:
+            print("error")
+
+if menu==5:
+    for i in range(20):
+        number=input("please enter number\nif youre done write stop:")
+        if number=="stop":
+            result=top5(all_numbers)
+            print(result)
+            break
+        if number.isdigit():    
+            all_numbers.append(float(number))
+        
+        else:
+            print("error")
+            
+if menu==6:
+    for i in range(20):
+        number=input("please enter number\nif youre done write stop:")
+        if number=="stop":
+            result=last5(all_numbers)
+            print(result)
+            break
+        if number.isdigit():    
+            all_numbers.append(float(number))
+        
+        else:
+            print("error")
+            
+    
+
+
+    
+    
+            
+
+    
+
+        
     
     
     

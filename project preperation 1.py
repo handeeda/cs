54a9434e8a66cc41cbe13fe240dc818668e19e24
handeeda <144910880+handeeda@users.noreply.github.com>
2024-09-09 12:00:44 +0100

    Add files via upload

--- project preperation 1.py
+++ project preperation 1.py
@@ -43,77 +43,70 @@ def last5(num):
             return list1
     
     
-        
+def text(def1):
+    f=open("C:/Users/23HOzdemir.ACC/Desktop/numbers.txt",'r')
     
+    for line in f:
+        
+        for i in line:
+            if i==" ":
+               continue 
+            else:
+                a=int(i)
+                text_num.append(a)
+            
+            
+            
+        
+        result=def1(text_num)
+        return result
+    f.close()
+
+
+def inputt(deff):
+    for i in range(20):
+        number=input("please enter youre numbers\nonly 20 or less\nenter number\nif youre done write stop:")
+        if number=="stop":
+            resultt=deff(all_numbers)
+            return resultt
+            break
+        if number.isdigit():    
+            all_numbers.append(float(number))
+        
+        else:
+            print("error")
             
         
         
     
 
 all_numbers=[]
+text_num=[]
+msg=("for this menu numbers have already been chosen")
 menu=int(input("1.mean\n2.median\n3.mode\n4.frequency\n5.top 5 numbers\n6.last 5 numbers\nchoose from the menu:"))
-print("please enter youre numbers\nonly 20 or less:")
+
 if menu==1:
-    for i in range (20):
-        
-        number=input("please enter number\nif youre done write stop:")
-        if number=="stop":
-            
-            result=mean(all_numbers)
-            print(result)
-            break
-        
-            
-        if number.isdigit():    
-            all_numbers.append(float(number))
-        
-            
-        else:
-            print("error")
+    print(msg)
+    r=text(mean)
+    print(r)
 if menu==2:
-    for i in range(20):
-        number=input("please enter number\nif youre done write stop:")
-        if number=="stop":
-            result=median(all_numbers)
-            print(result)
-            break
-        
-            
-        if number.isdigit():    
-            all_numbers.append(float(number))
-        
-        else:
-            print("error")
-
+    print(msg)
+    r=text(median)
+    print(r)
+           
 if menu ==3:
-    for i in range(20):
-        number=input("please enter number\nif youre done write stop:")
-        if number=="stop":
-            result=mode(all_numbers)
-            print(result)
-            break
-        if number.isdigit():    
-            all_numbers.append(float(number))
+    print(msg)
+    r=text(mode)
+    print(r)
         
-        else:
-            print("error")
-
+       
 if menu==4:
-    for i in range(20):
-        number=input("please enter number\nif youre done write stop:")
-        if number=="stop":
-            result=frequency(all_numbers)
-            print(result)
-            break
-        if number.isdigit():    
-            all_numbers.append(float(number))
-        
-        else:
-            print("error")
+    r=inputt(frequency)
+    print(r)
 
 if menu==5:
     for i in range(20):
-        number=input("please enter number\nif youre done write stop:")
+        number=input("please enter youre numbers\nonly 20 or less\nenter number\nif youre done write stop:")
         if number=="stop":
             result=top5(all_numbers)
             print(result)
@@ -126,7 +119,7 @@ if menu==5:
             
 if menu==6:
     for i in range(20):
-        number=input("please enter number\nif youre done write stop:")
+        number=input("please enter youre numbers\nonly 20 or less\nenter number\nif youre done write stop:")
         if number=="stop":
             result=last5(all_numbers)
             print(result)

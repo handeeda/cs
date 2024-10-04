0532b43fe72f6e2dc13baa50fd650f435fbe3488
handeeda <144910880+handeeda@users.noreply.github.com>
2024-10-04 12:57:54 +0100

    Add files via upload

--- reading and writing files1.py
+++ reading and writing files1.py
@@ -1,3 +1,4 @@
+"""
 menu=("1.create a new file\n2. display the file\n3.add a new item to the file")
 print(menu)
 selection=input("select:")
@@ -37,3 +38,23 @@ while a==1:
         selection=input("select:")
 
 print("____________________")
+
+
+f=open("book1.csv","w")
+
+mybooklist={"0":{"book":"to kill a mockingbird","author":"harper lee","year released":"1960"},"1":{"book":"a brief history of time","author":"stephen hawking","year released":"1988"},"2":{"book":"the great gatsby","author":"f.scott fitzgerald","year released":"1922"},"3":{"book":"the man who mistook his wife for a hat","author":"oliver sacks","year released":"1985"},"4":{"book":"pride and prejudice","author":"jane austen","year released":"1813"}}
+for i in mybooklist:
+    f.write(i)
+    for key,value in mybooklist[i].items():
+
+        f.write(",")
+        f.write(value)
+    f.write("\n")
+f.close()
+"""
+print("______________")
+
+
+
+
+

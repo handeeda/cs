b5e37940e0a1ed25a91b8abf84c3d7670c7aee56
handeeda <144910880+handeeda@users.noreply.github.com>
2024-10-11 12:59:06 +0100

    Add files via upload

--- user account program.py
+++ user account program.py
@@ -1,73 +1,81 @@
 user_ID=[]
-a=0
-s=0
+not_finished=False
+password_line=False
 menu=int(input("1)create a new user ID:\n2)change password:\n3)display all user IDs:\n4)quit:"))
-while s==0:
+
+while not not_finished:
+    
     if menu==1:
         ID=int(input("write a new ID:"))
-    #while menu==1:
-        #ID=int(input("write a new ID:"))
-    
-    #while menu==1:
-        for i in range(1):
+        y=False
+        while not y:
             person=[]
             if ID in user_ID:
                 print("the ID is already in the list")
                 ID=int(input("write a new ID:"))
             else:
                 person.append(ID)
+                
                 password=input("enter a password with at least 8 characters:")
-                password_len=len(password)
+                while not password_line:
+                    password_len=len(password)
+                    if password_len<8:
+                        print("youre password is too short..")
+                        password=input("enter a password with at least 8 characters:")
+                    else:
+                        print("youre password is valid!!")
+                        password_line=True
         
-                if password_len<8:
-                    print("youre password is too short..")
-                    password=input("enter a password with at least 8 characters:")
-        
-                elif password_len>=8:
+                if password_len>=8:
                     person.append(password)
                     #print(person)
                     print("youre new ID has been created")
                     user_ID.append(person)
-                #print(user_ID)
+                    print(user_ID)
+                    y=True
+                    #print(user_ID)
                     menu=int(input("1)create a new user ID:\n2)change password:\n3)display all user IDs:\n4)quit:"))            
             
-                else:
-                    print("youre password is invalid..")
-                    password=input("enter a password with at least 8 characters:")
 
     if menu==2:
-    #ID=int(input("write your ID:"))
-        ID=int(input("write your ID:"))
-    #while menu==2:
+        #ID=int(input("write your ID:"))
         
-        #a=0
-        for i in user_ID:
-            for p in i:
+        y=False
+        password_loop=False
+        #while menu==2:
+        
+            #a=0
+        while not y:
+            ID=int(input("write your ID:"))
+            for i in user_ID:
                 if ID in i:
-                    password=input("enter a password with at least 8 characters:")
-                    if password_len<8:
-                       print("youre password is too short..")
-                       password=input("enter a password with at least 8 characters:")
-        
-                    elif password_len>=8:
-                        for p in i:
-                            if p==ID:
-                                index=i.index(p)
-                                del i[index+1]
-                                i.insert(index+1,password)
-                                print(user_ID)
-                                print("your password has been changed!!")
-                        menu=int(input("1)create a new user ID:\n2)change password:\n3)display all user IDs:\n4)quit:"))            
-            
-                    else:
-                        print("youre password is invalid..")
+                    while not password_loop:
                         password=input("enter a password with at least 8 characters:")
+                        password_len=len(password)
+                        if password_len<8:
+                        
+                            print("youre password is too short..")
+                            #password=input("enter a password with at least 8 characters:")
+        
+                        else:
+                        
+                            for p in i:
+                                if p==ID:
+                                    index=i.index(p)
+                                    del i[index+1]
+                                    i.insert(index+1,password)
+                                    print(user_ID)
+                                    print("your password has been changed!!")
+                            menu=int(input("1)create a new user ID:\n2)change password:\n3)display all user IDs:\n4)quit:"))            
+                            y=True
+                        #else:
+                            #print("youre password is invalid..")
+                            #password=input("enter a password with at least 8 characters:")
             
         
-                else:
-                    #a=a+1
-                    print("this ID doesnt exist..")
-                    ID=int(input("write your ID:"))
+            if not y:
+                print("this ID doesnt exist..")
+                
             
             
 

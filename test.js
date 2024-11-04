b3a33e5531c2b334595a82eb614184d169330772
handeeda <144910880+handeeda@users.noreply.github.com>
2024-11-04 12:01:53 +0000

    Add files via upload

---
+++ test.js
@@ -0,0 +1,44 @@
+console.log("Hello World");
+//comments are ignored by the computer
+//two forward slashes are single line comments
+/*
+This is a 
+multi line comment
+*/
+//you can store data in variables
+//to declare a variable use the let keyword
+let myVariable = 5;
+console.log(myVariable);
+//text is written  in quotes
+let myName = "John";
+let myOtherName = "Doe";
+console.log(myName,myOtherName);
+//varibales are case sensitive
+let myname = "Bob"; //this is different to myName
+console.log(myName,myname);
+//variables must start with a letter [a-zA-Z]
+//underscore_or dollar sign $
+$some_variable = 200;
+_someOtherVariable = 500;
+total = $some_variable = _someOtherVariable;
+console.log(total);
+/*
+math operators are like ordinary math
++ add
+- substract
+* multiply
+/ divide
+% remainder
+*/
+
+let addition = 4+4;
+let substraction = 5-4;
+let multiply = 4*3;
+let divide = 4/2;
+//reaminder gives the remainder after division
+let reaminder = 6%4;
+//the exponent or power is **
+let four_cubed = 4**3;
+//the increment operator ++
+let x = 5;
+

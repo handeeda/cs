1c026ccd0774838d4f7e250a9a51f03cdc66391c
handeeda <144910880+handeeda@users.noreply.github.com>
2024-11-04 12:01:42 +0000

    Add files via upload

---
+++ javascript first exercises.js
@@ -0,0 +1,34 @@
+let a=1;
+let b=2.5;
+let c=3;
+console.log(a,b,c);
+a=a+b;
+b=b+c;
+console.log(a,b,c);
+
+let n=7;
+let num1=n;
+let num2=n+1;
+let num3=n+2;
+console.log(num1,num2,num3);
+
+let radius=3;
+let pi=3.14;
+answer=pi*radius**2;
+console.log(answer);
+
+let base=2;
+let height=6;
+area=1/2*base*height;
+console.log(area);
+
+n=2;
+console.log(n)
+console.log(n**2)
+console.log(n**3)
+console.log(n**4)
+
+height=7
+let inch=height/2.54
+let foot=inch/12
+console.log(foot)
